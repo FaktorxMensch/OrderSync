5deb3ab8d8c0a118e63b6732fd50a44a8239b0c2
Dominik <dominik.ziegenhagel@gmail.com>
2023-12-24 19:34:10 +0100

    move from bun to yarn

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <script setup>
 const {
-  data, refresh
+  data, refresh, loading, pending
 } = await useLazyFetch('https://fatima-dresden.de/wp-content/plugins/fatima_api_status_plugin/api.php?api_key=ad31f2b0-8b7c-11eb-8dcd-0242ac17841741897830003')
 
-setInterval(refresh, 1000 * 60 * 5)
+// setInterval(refresh, 1000 * 60 * 5)
 </script>
 
 <template>
@@ -11,11 +11,12 @@ setInterval(refresh, 1000 * 60 * 5)
     <v-app-bar>
       <v-toolbar-title>Bestellungen</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-chip color="green" text-color="white">{{ new Date(data.request_time).toLocaleString('de-de') }}</v-chip>
+<!--      <v-chip color="green" text-color="white">{{ new Date(data?.request_time).toLocaleString('de-de') }}</v-chip>-->
       <v-btn icon="mdi-refresh" @click="refresh"/>
     </v-app-bar>
     <v-main>
-      <bestellungs-liste :bestellungen="data.bestellungen" @refresh="refresh"/>
+<!--      <v-progress-circular v-if="data.pending" indeterminate/>-->
+<!--      <bestellungs-liste :bestellungen="data.bestellungen" @refresh="refresh" v-else/>-->
     </v-main>
   </v-app>
 </template>
