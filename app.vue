09776f0a18968724c5e3410df93844af38f0a62d
Dominik <dominik.ziegenhagel@gmail.com>
2023-12-24 17:45:55 +0100

    add everything

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
+<script setup>
+const {
+  data,
+  refresh
+} = await useLazyFetch('https://fatima-dresden.de/wp-content/plugins/fatima_api_status_plugin/api.php?api_key=ad31f2b0-8b7c-11eb-8dcd-0242ac17841741897830003')
+
+setInterval(refresh, 1000 * 60 * 5)
+</script>
 <template>
-  <div>
-    <NuxtWelcome />
+  <h1>Bestellungen</h1>
+  <div class="bestellungen">
+  <bestellung v-for="item in data.bestellungen" :key="item.id" :item="item"/>
   </div>
+  Refreshed: {{ data.request_time }}
+
+  <button @click="refresh">Refresh</button>
 </template>
---
+++ components/bestellung.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: bestellung
+  </div>
+</template>
+
+<style scoped></style>
