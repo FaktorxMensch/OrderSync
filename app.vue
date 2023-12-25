07b16d40dc8613e4f6b75bc0715425f02033575a
Dominik <dominik.ziegenhagel@gmail.com>
2023-12-25 10:58:37 +0100

    dat bestellungen might be null

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Actionbar from "~/components/actionbar.vue";
+
 const {
   data, refresh, loading, pending
 } = await useLazyFetch('https://fatima-dresden.de/wp-content/plugins/fatima_api_status_plugin/api.php?api_key=ad31f2b0-8b7c-11eb-8dcd-0242ac17841741897830003')
@@ -26,41 +28,16 @@ const addRandomOrder = async () => {
 
 setInterval(refresh, 1000 * 60 * 5)
 
-const orders = useOrdersStore()
-const bestellung = computed(() => orders.bestellung)
 </script>
-
 <template>
   <v-theme-provider theme="dark">
     <v-app>
-      <v-app-bar density="compact">
-        <template v-if="bestellung?.id" density="compact">
-          <v-btn icon="mdi-chevron-left" @click="orders.closeBestellung()"/>
-          <v-toolbar-title>Bestellung #{{ bestellung.id }}</v-toolbar-title>
-          <v-btn :href="`tel:${bestellung.rufnummer}`" prepend-icon="mdi-phone">Anrufen</v-btn>
-        </template>
-        <template v-else>
-          <v-toolbar-title>Übersicht</v-toolbar-title>
-          <v-btn
-              prepend-icon="mdi-refresh"
-              @click="refresh"
-              text-color="white">Stand {{
-              new Date(data?.request_time).toLocaleTimeString('de-de', {
-                hour: '2-digit',
-                minute: '2-digit'
-              })
-            }}
-          </v-btn>
-        </template>
-      </v-app-bar>
+      <actionbar/>
       <v-main v-if="data !== null">
         <v-progress-circular v-if="data.pending" indeterminate/>
         <bestellungen :bestellungen="data.bestellungen" @refresh="refresh" v-else/>
+        <notification-sound :bestellungen="data.bestellungen"/>
       </v-main>
-      <notification-sound :bestellungen="data.bestellungen"/>
     </v-app>
   </v-theme-provider>
 </template>
-
-<style>
-</style>
---
+++ components/actionbar.vue
@@ -0,0 +1,26 @@
+<script setup>
+const orders = useOrdersStore()
+const bestellung = computed(() => orders.bestellung)
+</script>
+<template>
+  <v-app-bar density="compact">
+    <template v-if="bestellung?.id" density="compact">
+      <v-btn icon="mdi-chevron-left" @click="orders.closeBestellung()"/>
+      <v-toolbar-title>Bestellung #{{ bestellung.id }}</v-toolbar-title>
+      <v-btn :href="`tel:${bestellung.rufnummer}`" prepend-icon="mdi-phone">Anrufen</v-btn>
+    </template>
+    <template v-else>
+      <v-toolbar-title>Übersicht</v-toolbar-title>
+      <v-btn
+          prepend-icon="mdi-refresh"
+          @click="refresh"
+          text-color="white">Stand {{
+          new Date(data?.request_time).toLocaleTimeString('de-de', {
+            hour: '2-digit',
+            minute: '2-digit'
+          })
+        }}
+      </v-btn>
+    </template>
+  </v-app-bar>
+</template>
